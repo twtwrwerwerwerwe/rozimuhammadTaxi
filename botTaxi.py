257ef0f77aa9b308e8c5b2f2ec8855cae3627e46
Rozimuhammad Akramjonov <rozimuhammad@example.com>
2025-12-14 11:48:59 +0500

    yangiladim

--- botTaxi.py
+++ botTaxi.py
@@ -137,7 +137,7 @@ async def handler(event):
             f"📍 <b>Guruh:</b> {group_display}\n\n"
             f"👤 <b>Egasi:</b> {owner_display}\n\n"
             f"📞 <b>Telefon:</b> {phone_display}\n\n"
-            f"🔗 <b>Profilga o'tish:</b>{profile_link}"
+            f"🔗 <b>Profilga o'tish:</b> {profile_link}"
         )
 
         for target_id in TARGET_CHAT_IDS:
