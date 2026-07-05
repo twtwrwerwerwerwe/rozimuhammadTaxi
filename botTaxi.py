d16acd292d96b210a9bb114380dd5edbb33c0f1b
Rozimuhammad <sizningemailingiz@gmail.com>
2026-07-05 21:36:53 +0500

    yangilandi

--- botTaxi.py
+++ botTaxi.py
@@ -122,8 +122,7 @@ KEYWORDS = [
     "4 odam bor", "2ta ayol bor", "katta yoshli ayol bor", "bir qiz bir bola bor", "srochni yuradigan taxi kerak",
     "kim yuryabdi", "toshkentga ketaman", "bagdodga ketishi kerak", "bagdodan 1kishi bor", "bog'doddan 2kishi", "yuk bor",
     "1 kiwi bor edi", "2 kiwi bor edi", "3 kiwi bor edi", "4 kiwi bor edi", "1 kishi bor edi", "2 kishi bor edi", "3 kishi bor edi", "4 kishi bor edi", 
-    "1 kishi bor ekan", "2 kishi bor ekan", "3 kishi bor ekan", "4 kishi bor ekan", "1 kiwi bor ekan", "2 kiwi bor ekan", "3 kiwi bor ekan", "4 kiwi bor ekan",
-    "1 kishi bor edi", "2 kishi bor edi", "3 kishi bor akalar"
+    "1 kishi bor ekan", "2 kishi bor ekan", "3 kishi bor ekan", "4 kishi bor ekan", "1 kiwi bor ekan", "2 kiwi bor ekan", "3 kiwi bor ekan", "4 kiwi bor ekan"
 ]
 
 KEYWORDS_RE = re.compile("|".join(re.escape(k) for k in KEYWORDS), re.IGNORECASE)
@@ -188,7 +187,7 @@ async def handler(event):
         phone_display = phone if phone else "Berkitilgan"
 
         message_text = (
-            f"🔈  <b>Yangi elon!</b>\n\n"
+            f"🔈  <b>Elon topildi!</b>\n\n"
             f"📝  <b>Elon : </b> {text}\n\n"
             f"📍  <b>Guruh : </b> {group_display}\n\n"
             f"👤  <b>User : </b> {owner_display}\n\n"
